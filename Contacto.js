d21aeaf47ac358821eb63720799719309aa1b228
ANGELA <angela@imac-graciela.home>
2025-11-25 12:07:38 +0100

    se añade mensaje al responder formulario

---
+++ Contacto.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contacto-form');
+    const formContenedor = document.getElementById('formulario-contenedor');
+    const mensajeExito = document.getElementById('mensaje-exito');
+    const volverBtn = document.getElementById('volver-btn');
+
+    form.addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+
+        formContenedor.style.display = 'none';
+
+        mensajeExito.style.display = 'flex';
+    });
+
+    volverBtn.addEventListener('click', function() {
+        mensajeExito.style.display = 'none';
+
+        formContenedor.style.display = 'block';
+
+        form.reset();
+    });
+});
